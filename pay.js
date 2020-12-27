79c999ac7959f8be4157557d1cd74229b36ba0ac
Li_benwei <51532767+Li-Zi-stu@users.noreply.github.com>
2020-12-27 12:00:35 +0800

    Add files via upload

---
+++ pay.js
@@ -0,0 +1,8 @@
+var setting = {
+	//QQ
+        qqUrl: "https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&f=wallet&a=1&ac=CAEQvPHd7QUYhOCf_wU%3D_xxx_sign&u=1572305084&n=%E6%9D%8E%E6%9C%AC%E4%BC%9F	",
+    //WrChat
+		wechatUrl: "wxp://f2f0JGpYgWLAj4lZ2CPzb6WhJkXMlDTONw6r",
+    //ali
+		aliUrl: "https://qr.alipay.com/fkx18253pqacudssd1val5a"
+}
